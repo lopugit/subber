ac826b904109159064e405177f0e19d2733f56da
Nikolaj Frey <nikolaj@thirstcreative.com.au>
2021-12-28 22:58:12 +1100

    main

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const serveStatic = require('serve-static')
-const history = require('connect-history-api-fallback')
 const enforce = require('express-sslify')
 
 const port = process.env.PORT || 5000
@@ -8,8 +6,8 @@ const port = process.env.PORT || 5000
 const app = express()
 
 app.use(enforce.HTTPS({ trustProtoHeader: true }))
-app.use(history())
-/* eslint-disable */
-app.use(serveStatic(__dirname + '/dist/spa'))
-/* eslint-enable */
 app.listen(port)
+
+app.get('*', (req, res) => {
+  res.redirect('https://ytsubber.com')
+})
