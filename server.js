fe8ad5f0f17d8ba3f7e28bfeb4a8a0c4a7d58ca7
Nikolaj Frey <nikolaj@thirstcreative.com.au>
2021-12-28 21:05:08 +1100

    main

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const history = require('connect-history-api-fallback')
+const enforce = require('express-sslify')
+
 const port = process.env.PORT || 5000
 
 const app = express()
 
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 app.use(history())
 /* eslint-disable */
 app.use(serveStatic(__dirname + '/dist/spa'))
